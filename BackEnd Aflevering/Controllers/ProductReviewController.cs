54f1bbb4424b03f5822acf1862f5604a24fce75c
Esben Kiel <Esbenks@gmail.com>
2019-03-10 20:16:32 +0100

    First commit

---
+++ BackEnd Aflevering/Controllers/ProductReviewController.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_Aflevering.Controllers
+{
+    public class ProductReviewController : Controller
+    {
+        // GET: ProductReview
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BackEnd Aflevering/Models/ProductReviewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackEnd_Aflevering.Models
+{
+    public class ProductReviewModel
+    {
+        private Guid guid { get; set; }
+        public String Name { get; set; }
+        public string Product { get; set; }
+        public int Score { get; set; } = 0; 
+
+
+    }
+} 
